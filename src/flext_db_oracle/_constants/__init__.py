588551b4ea5df2b58c9f3211c16d3f51116a56d2
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-20 16:56:35 -0300

    chore(gen): converge fleet projections (post integration landing)

--- src/flext_db_oracle/_constants/__init__.py
+++ src/flext_db_oracle/_constants/__init__.py
@@ -12,8 +12,7 @@ if TYPE_CHECKING:
     from .base import FlextDbOracleConstantsBase
     from .exceptions import FlextDbOracleConstantsExceptions
 __all__: tuple[str, ...] = (
-    "FlextDbOracleConstantsBase",
-    "FlextDbOracleConstantsExceptions",
+    "FlextDbOracleConstantsBase", "FlextDbOracleConstantsExceptions",
 )
 
 _LAZY_IMPORTS = MappingProxyType(
